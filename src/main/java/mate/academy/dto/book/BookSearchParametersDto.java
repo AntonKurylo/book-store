e89bb6450e6bb863673046e8b70ff17e07c20b76
Anton Kurylo <antonkurilo9@gmail.com>
2024-08-15 12:55:19 +0300

    change the logic of using Specification for the Book entity

--- src/main/java/mate/academy/dto/book/BookSearchParametersDto.java
+++ src/main/java/mate/academy/dto/book/BookSearchParametersDto.java
@@ -1,6 +1,10 @@
 package mate.academy.dto.book;
 
+import jakarta.validation.constraints.Size;
+
 public record BookSearchParametersDto(
-        String[] titles,
-        String[] authors) {
+        @Size(max = 128, message = "Title must be less than 128 characters")
+        String title,
+        @Size(max = 64, message = "Author must be less than 64 characters")
+        String author) {
 }
--- src/main/java/mate/academy/repository/SpecificationBuilder.java
+++ src/main/java/mate/academy/repository/SpecificationBuilder.java
@@ -4,5 +4,5 @@ import mate.academy.dto.book.BookSearchParametersDto;
 import org.springframework.data.jpa.domain.Specification;
 
 public interface SpecificationBuilder<T> {
-    Specification<T> build(BookSearchParametersDto searchParametersDto);
+    Specification<T> build(BookSearchParametersDto searchParameters);
 }
