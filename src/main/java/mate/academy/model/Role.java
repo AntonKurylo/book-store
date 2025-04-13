fff4934823e50c821296df9c6c4539f569cf1e5c
Anton Kurylo <antonkurilo9@gmail.com>
2025-04-14 01:16:05 +0300

    add docker to the project

--- src/main/java/mate/academy/model/Role.java
+++ src/main/java/mate/academy/model/Role.java
@@ -20,7 +20,7 @@ public class Role implements GrantedAuthority {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(nullable = false, unique = true, columnDefinition = "varchar")
+    @Column(nullable = false, unique = true, columnDefinition = "VARCHAR(64)")
     @Enumerated(EnumType.STRING)
     private RoleName name;
 
